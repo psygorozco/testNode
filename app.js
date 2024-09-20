a9b03f8acbd6c593a59b805febded0519cb5ab2e
psygorozco <162739079+psygorozco@users.noreply.github.com>
2024-09-20 13:21:51 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const app = express();
 const port = process.env.PORT || 3001;
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
+// Usar assistantId de las variables de entorno
+const assistantId = process.env.ASSISTANT_ID;
+
 app.use(cors());
 app.use(express.json());
 
@@ -14,7 +17,7 @@ app.get("/", (req, res) => {
 });
 
 app.post("/openai", async (req, res) => {
-  const { prompt, threadId, assistantId } = req.body;
+  const { prompt, threadId } = req.body;
   if (!prompt) return res.status(400).json({ error: "El campo 'prompt' es necesario" });
 
   try {
